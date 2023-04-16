700893b6742fae7ceec3946054b073066e0f71c1
TheProgrammer <mxolisinkosi2360@gmail.com>
2023-04-16 11:03:34 +0200

    added functionality for the guessing part

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from flask import Flask
-
+import random
 app = Flask(__name__)
+
+RANDOM_NUMBER = random.randint(0, 9)
 
 
 @app.route("/")
@@ -8,6 +10,23 @@ def hello_world():
     return '<h1>Guess a number between 0 and 9</h1>' \
            '<img src="https://media.giphy.com/media/3o7TKRVBGOSdSODGJa/giphy.gif" width="500" height="500"> '
 
+
+
+@app.route("/<int:number>")
+def take_a_guess(number):
+    if number == RANDOM_NUMBER:
+        # win
+        value = '<h1 style="color:red;">You got me!</h1>' \
+                '<img src="https://media.giphy.com/media/3o7abKhOpu0NwenH3O/giphy.gif" width="500" height="500">'
+    elif number < RANDOM_NUMBER:
+        # less than
+        value = '<h1 style="color:red;">Too Low, try again</h1>' \
+                '<img src="https://media.giphy.com/media/mhxZiXPbpF8QmtJe7Q/giphy.gif" width="500" height="500">'
+    else:
+        # greater than
+        value = '<h1 style="color:purple;">Too high, try again</h1>' \
+                '<img src="https://media.giphy.com/media/79eQOjPPrisR9B2zy6/giphy.gif" width="500" height="500">'
+    return value
 
 if __name__ == "__main__":
     app.run(debug=True)
